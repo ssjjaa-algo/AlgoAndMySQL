d0f981f7369fc14fe0c2944586c727860c2a59b3
ssjjaa-algo <tjdwo6269@naver.com>
2023-06-13 15:47:18 +0900

    Add : BOJ1774 줄어드는 수 dfs 풀이

---
+++ src/Baekjoon/Main.java
@@ -0,0 +1,62 @@
+package Baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int count = 0;
+    static int[] arr = new int[10];
+    static List<String> stringList = new ArrayList<>();
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        if (N >= 1024) { // 9876543210이 1023번째로 등장하는 마지막 감소하는 수임.
+            System.out.println(-1);
+            return;
+        }
+
+        for (int i=1; i<=10; i++) {
+            dfs(0,i); // 자리수마다 dfs 시작
+        }
+
+        System.out.println(stringList.get(N - 1));
+
+        wr.close();
+        br.close();
+
+    }
+
+    private static void dfs(int start, int cnt) {
+
+        if (start == cnt) {
+
+            for (int i=0; i<cnt; i++) {
+                sb.append(arr[i]);
+            }
+            stringList.add(sb.toString());
+            sb.setLength(0);
+        }
+
+        for (int i=0; i< 10; i++) {
+
+            if (start == 0) {
+                arr[start] = i;
+                dfs(start + 1, cnt);
+            }
+
+            else {
+                if (arr[start - 1] > i) { // 이전의 수가 현재 수보다 크다면
+                    arr[start] = i;
+                    dfs(start + 1, cnt);
+                }
+            }
+
+        }
+    }
+
+
+}
