fbb09fd087553fc1d7471b11ebe009cbdd772485
김성재 <tjdwo6269@naver.com>
2023-07-28 08:33:12 +0900

    Add : BOJ11726 2xN 타일링 피보나치 풀이

---
+++ src/Baekjoon/BOJ11726.java
@@ -0,0 +1,24 @@
+package Baekjoon;
+
+import java.io.*;
+
+public class BOJ11726 {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N + 1];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <=N; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007 ;
+        }
+
+        System.out.println(dp[N]);
+
+    }
+}
