94bde03e58f7675fdf3a588f356521305ddac1d7
김성재 <tjdwo6269@naver.com>
2023-09-05 08:41:10 +0900

    Add : Programmers LV3 네트워크 bfs 풀이

---
+++ src/Programmers/Algorithm/네트워크(bfs).java
@@ -0,0 +1,41 @@
+package Programmers.Algorithm;
+
+import java.util.*;
+class Solution {
+
+    public int solution(int n, int[][] computers) {
+
+        int answer = 0;
+
+        boolean[] visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if(!visited[i]) {
+                answer++;
+                bfs(i,n,computers,visited);
+            }
+        }
+        return answer;
+    }
+
+    public void bfs(int v, int n, int[][] computers, boolean[] visited) {
+        Queue<Integer> q = new ArrayDeque<>();
+
+        visited[v] = true;
+        q.add(v);
+
+        while (!q.isEmpty()) {
+
+            int vertex = q.poll();
+            for (int i = 0; i < n; i++) {
+                if (computers[vertex][i] == 1 && !visited[i]) {
+                    q.add(i);
+                    visited[i] = true;
+                }
+            }
+        }
+
+    }
+
+
+}
