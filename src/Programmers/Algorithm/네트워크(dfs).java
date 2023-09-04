63f754ea7608ed46ab2778380768ba3abfebfcb1
김성재 <tjdwo6269@naver.com>
2023-09-05 08:29:43 +0900

    Add : Programmers LV3 네트워크 dfs 풀이

---
+++ src/Programmers/Algorithm/네트워크(dfs).java
@@ -0,0 +1,31 @@
+package Programmers.Algorithm;
+
+class Solution {
+
+    public int solution(int n, int[][] computers) {
+
+        int answer = 0;
+        boolean[] visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) { // 방문하지 않은 컴퓨터라면
+            if (!visited[i]) {
+                answer++;
+                dfs(i,n,computers,visited);
+            }
+        }
+
+        return answer;
+    }
+
+    public void dfs(int v, int n, int[][] computers, boolean[] visited) {
+
+        visited[v] = true;
+
+        for (int i = 0; i < n; i++) {
+            if (computers[v][i] == 1 && !visited[i]) {
+                dfs(i,n,computers,visited);
+            }
+        }
+    }
+
+}
