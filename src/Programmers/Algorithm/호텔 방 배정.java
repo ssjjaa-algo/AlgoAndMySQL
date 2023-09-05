080ee33396785db0e2cf3c0dbee895b2111baf1d
김성재 <tjdwo6269@naver.com>
2023-09-05 20:43:52 +0900

    Add : Programmers LV4 호텔 방 배정 Union-Find 풀이

---
+++ src/Programmers/Algorithm/호텔 방 배정.java
@@ -0,0 +1,33 @@
+package Programmers.Algorithm;
+
+import java.util.*;
+
+class Solution {
+
+    Map<Long,Long> map = new HashMap<>();
+
+    public long[] solution(long k, long[] room_number) {
+
+        int size = room_number.length;
+        long[] answer = new long[size];
+
+        for (int i = 0; i < size; i++) {
+            answer[i] = room(room_number[i]);
+        }
+
+        return answer;
+    }
+
+    public long room(long roomNumber) {
+        // 1. 해당 방에 들어갈 수 있는 경우
+        if (!map.containsKey(roomNumber)) {
+            map.put(roomNumber,roomNumber + 1);
+            return roomNumber;
+        }
+
+        long findEnterRoom = map.get(roomNumber);
+        long findEmptyRoom = room(findEnterRoom);
+        map.put(roomNumber,findEmptyRoom);
+        return findEmptyRoom;
+    }
+}
