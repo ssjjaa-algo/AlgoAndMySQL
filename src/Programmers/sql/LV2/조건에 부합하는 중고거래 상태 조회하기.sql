3b330fccc7f0565a5e132fcb554d5ad5220efc29
김성재 <tjdwo6269@naver.com>
2023-09-07 08:42:34 +0900

    Add : Programmers sql 조건에 부합하는 중고거래 상태 조회하기

---
+++ src/Programmers/sql/LV2/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+SELECT BOARD_ID, WRITER_ID, TITLE, PRICE,
+       CASE WHEN STATUS = "SALE" then "판매중"
+            WHEN STATUS = "RESERVED" then "예약중"
+            WHEN STATUS = "DONE" then "거래완료"
+           END STATUS
+FROM USED_GOODS_BOARD
+WHERE CREATED_DATE = '2022-10-05'
+ORDER BY BOARD_ID DESC;
