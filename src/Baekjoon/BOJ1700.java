ab1099764d867ddc1132d4f2ca756d8333779219
ssjjaa-algo <tjdwo6269@naver.com>
2024-01-05 17:42:49 +0900

    Add : BOJ1700 멀티탭 스케줄링 그리디 풀이

---
+++ src/Baekjoon/BOJ1700.java
@@ -0,0 +1,124 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/*
+1. 이미 플러그에 꽂혀있는 경우
+    if (set.contains(command)) continue
+
+2. 플러그에 꽂혀있지 않은 경우
+    if (!set.contains(command))
+    - 해당 위치에서 k개의 플러그를 확인한다.
+        for (int j = 현재 명령어 위치; j < 현재 명령어 위치 + k; j++)
+            if (set.contains(command[j]))
+                count[command[j]]++;
+    - 꽂혀있는 플러그 중 count[set의 원소]가 가장 작은 원소를 제거한다.
+
+ */
+public class BOJ1700 {
+
+    static int N,K;
+    static int[] command;
+    static int[] sequenceOfCommand;
+    static Set<Integer> set = new HashSet<>();
+    public static void main(String[] args) throws IOException {
+
+        init();
+        System.out.println(calculate());
+    }
+
+    private static int calculate() {
+
+        int cnt = 0;
+
+        for (int i = 0; i < N; i++) {
+            set.add(command[i]);
+        }
+
+        // print();
+
+        int currentCommand = 0;
+        int change = 0;
+        for (int i = N; i < K; i++) {
+            currentCommand = command[i];
+
+            if (set.size() < N || set.contains(currentCommand)) {
+                set.add(currentCommand);
+                continue;
+            }
+            change = changePlug(i);
+
+            set.remove(change);
+            set.add(currentCommand);
+            cnt++;
+        }
+
+        return cnt;
+    }
+
+    private static int changePlug(int idx) {
+
+        for (int i = idx; (i < K); i++) {
+
+            if (set.contains(command[i]) && sequenceOfCommand[command[i]] == 0) {
+                sequenceOfCommand[command[i]] = i;
+            }
+        }
+
+        Iterator<Integer> iter = set.iterator();
+
+        int num = 0;
+        int maxNum = 0;
+        int change = 0;
+        while (iter.hasNext()) {
+            num = iter.next();
+            if (sequenceOfCommand[num] == 0 ) {
+                change = num;
+                break;
+            }
+
+            if (maxNum < sequenceOfCommand[num]) {
+                maxNum = sequenceOfCommand[num];
+                change = num;
+            }
+        }
+
+        Arrays.fill(sequenceOfCommand,0);
+
+        // System.out.println("변화 대상 : " + change);
+        return change;
+    }
+
+    private static void print() {
+
+        Iterator iterator = set.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+        System.out.println();
+    }
+
+    private static void init() throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        N = Integer.parseInt(input[0]);
+        K = Integer.parseInt(input[1]);
+        command = new int[K + 1];
+        sequenceOfCommand = new int[K + 1];
+
+        input = br.readLine().split(" ");
+
+        for (int i = 0; i < K; i++) {
+            command[i] = Integer.parseInt(input[i]);
+        }
+    }
+}
